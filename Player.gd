04216005161883dd694220070e83c339afae4626
Mariolink528 <Tyler.Thraves@yahoo.com>
2020-01-15 13:25:36 -0800

    Collision working!

--- Player.gd
+++ Player.gd
@@ -1,35 +1,55 @@
 extends KinematicBody2D
 var screen_size
-var speed = 250
+const MAX_SPEED = 200
+const ACCELERATION = 50
+var motion = Vector2()
+const UP = Vector2(0, -1)
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	pass
 
-func _process(delta):
-	var velocity = Vector2()
+
+func _physics_process(delta):
+	
+	var friction = false
 	if Input.is_action_pressed("ui_right"):
-		velocity.x +=1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -=1
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		
+		
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		
+		
+	else: 
+		 motion.x = 0
+		 friction = true
 	if Input.is_action_pressed("ui_down"):
-		velocity.y +=1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -=1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
+		motion.y = min(motion.y + ACCELERATION, MAX_SPEED)
+			 
+	elif Input.is_action_pressed("ui_up"):
+		motion.y = max(motion.y - ACCELERATION, -MAX_SPEED)
 	else:
-		$AnimatedSprite.stop()
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "right"
-		$AnimatedSprite.flip_v = false
-		$AnimatedSprite.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite.animation = "Up"
-		$AnimatedSprite.flip_v = velocity.y > 0
+		motion.y = 0 
+		friction = true
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = min(motion.y + ACCELERATION, MAX_SPEED)
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
+	
+	if friction == true:
+		motion.x = lerp(motion.x, 0, 0.05)
+	motion = move_and_slide(motion, UP)
 
-
+func _on_Player_body_entered(body):
+	if Input.is_action_pressed("ui_right"):
+		move_local_x( -9)
+	if Input.is_action_pressed("ui_left"):
+		move_local_x( 9)
+	if Input.is_action_pressed("ui_down"):
+		move_local_y( -9)
+	if Input.is_action_pressed("ui_up"):
+		move_local_y( 9)
+	print("Works");
