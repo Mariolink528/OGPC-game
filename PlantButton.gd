69b8d90fe5a50e242c1165e1508bc0666f60a05d
Mariolink528 <Tyler.Thraves@yahoo.com>
2020-01-22 13:14:37 -0800

    button follows mouse

--- PlantButton.gd
+++ PlantButton.gd
@@ -13,6 +13,7 @@ func _ready():
 func _on_Planter_mouse_entered():
 	show()
 	print("yay");
+	set_position(Vector2(get_global_mouse_position().x, get_global_mouse_position().y))
 
 
 func _on_Planter_mouse_exited():
